056e51a599942d0d2daf9c83336f4373d92d5038
Litvinov Alexander <bumble-bee83@mail.ru>
2018-09-13 10:56:10 +0300

    values > 0

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
   var counter = 0;
   for(var i = 0; i < preferences.length; i++){
-    if ((preferences[i] <= preferences.length) && (preferences[preferences[i] - 1] <= preferences.length) && (preferences[preferences[preferences[i] - 1] - 1] <= preferences.length)){
+    if ((preferences[i] > 0 && preferences[i] <= preferences.length)
+         && (preferences[preferences[i] - 1] > 0 && preferences[preferences[i] - 1] <= preferences.length)
+         && (preferences[preferences[preferences[i] - 1] - 1] > 0 && preferences[preferences[preferences[i] - 1] - 1] <= preferences.length)){
       var curIndex = i;
       if (preferences[preferences[preferences[i] - 1] - 1] === ++curIndex){
         counter++;
