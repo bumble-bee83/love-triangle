7013f0dd4e5377d48391d1817c44cc4ae044972c
Litvinov Alexander <bumble-bee83@mail.ru>
2018-09-13 10:44:43 +0300

    love-triangle

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var counter = 0;
+  for(var i = 0; i < preferences.length; i++){
+    if ((preferences[i] <= preferences.length) && (preferences[preferences[i] - 1] <= preferences.length) && (preferences[preferences[preferences[i] - 1] - 1] <= preferences.length)){
+      var curIndex = i;
+      if (preferences[preferences[preferences[i] - 1] - 1] === ++curIndex){
+        counter++;
+      }
+    }
+  }
+  return Math.floor(counter / 3);
 };
